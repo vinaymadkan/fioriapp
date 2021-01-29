ec5310b9f0d8c628294b150e011f5d5641cd0a39
vinaymadkan <vinay.madkan@clocwork.ca>
2021-01-29 17:04:18 +0000

    Initial commit.

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("GR.POST.FOR.PO.GR.POST.FOR.PO.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"GR/POST/FOR/PO/GR/POST/FOR/PO/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
